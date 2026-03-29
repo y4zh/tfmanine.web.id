74893510ba8461dfeb4941d2bbc58cdaa886cca8
y4zh <134795112+y4zh@users.noreply.github.com>
2026-03-29 11:28:05 +0700

    Update data.js

--- assets/js/data.js
+++ assets/js/data.js
@@ -503,8 +503,68 @@ window.appData = {
                 ops: "05.00 - 21.00",
                 headway: "20 Menit"
             },
-            routeMapImage: "https://smk.transjakarta.co.id/aset/berkas/rute/11P-20240709.jpg",
-            directions: [{ name: "Arah Walikota", stops: [{ name: "Rusun Pondok Bambu" }, { name: "Walikota Jaktim" }] }]
+            directions: [
+                {
+                    name: "Arah Walikota Jakarta Timur 1",
+                    stops: [
+                        { name: "Rusun Pondok Bambu", transfers: ["JAK 85"], isActive: true, label: "Terdekat" },
+                        { name: "Jln. Teluk Mandar 1", transfers: ["JAK 02", "JAK 85"], isActive: true },
+                        { name: "Gereja Santa Anna 1", transfers: ["JAK 02", "JAK 85"] },
+                        { name: "Duren Sawit", transfers: ["JAK 02", "JAK 26"] },
+                        { name: "Baladewa Residence", transfers: ["11Q", "JAK 42"] },
+                        { name: "Masjid Jami Al Barkah 1", transfers: ["11Q", "JAK 42"] },
+                        { name: "Jln. Serdang", transfers: ["11Q", "JAK 39", "JAK 42"], isActive: true },
+                        { name: "Jln. Swadaya Raya", transfers: ["11Q", "JAK 39"] },
+                        { name: "Masjid Jami Nurul Ain 1", transfers: ["11Q"] },
+                        { name: "Jln. H. Aman", transfers: ["11Q", "JAK 99"] },
+                        { name: "Wisma Atlet Radin Inten", transfers: ["11Q", "JAK 99"] },
+                        { name: "Kasi Dikdas 1", transfers: [] },
+                        { name: "Dinas Kebersihan Duren Sawit", transfers: ["11Q", "JAK 39", "JAK 99"] },
+                        { name: "Buaran Plaza", transfers: ["11Q", "JAK 39"] },
+                        { name: "Taman Buaran Indah", transfers: ["11Q", "JAK 39"] },
+                        { name: "Flyover Radin Inten 2", transfers: ["11Q", "JAK 39"], icons: ["icon-train.svg"], halteInfo: { type: "integrasi", halte: ["Simpang Buaran"], routes: ["11", "11M"] }, stationIntegration: { station: "Buaran", trainLines: ["C | Cikarang"] } },
+                        { name: "Jln. I Gusti Ngurah Rai 1", transfers: [] },
+                        { name: "Kampung Jembatan Ngurah Rai", transfers: [] },
+                        { name: "Perumnas Klender 2", transfers: ["11Q", "JAK 39"] },
+                        { name: "Sekolah PGRI Penggilingan", transfers: [] },
+                        { name: "Penggilingan 1", transfers: ["11B", "11Q", "11R", "JAK 100"] },
+                        { name: "Pengadilan Negeri Jaktim 1", transfers: [] },
+                        { name: "Jln. Dr. Sumarno", transfers: [] },
+                        { name: "Walikota Jakarta Timur 1", transfers: ["11B", "11Q", "JAK 100"] }
+                    ]
+                },
+                {
+                    name: "Arah Rusun Pondok Bambu",
+                    stops: [
+                        { name: "Walikota Jakarta Timur 1", transfers: ["11B", "11Q", "JAK 100"] },
+                        { name: "Kodim 0505 1", transfers: [] },
+                        { name: "Kodim 0505 2", transfers: [] },
+                        { name: "Walikota Jakarta Timur 2", transfers: ["11B", "11Q", "JAK 100"] },
+                        { name: "Pengadilan Negeri Jaktim 2", transfers: [] },
+                        { name: "Penggilingan 2", transfers: ["11B", "11Q", "11R", "JAK 100"] },
+                        { name: "Sbr. Sekolah PGRI Penggilingan", transfers: [] },
+                        { name: "Perumnas Klender 1", transfers: ["11Q"] },
+                        { name: "Rusun Klender", transfers: ["11Q"] },
+                        { name: "Jln. I Gusti Ngurah Rai 2", transfers: [] },
+                        { name: "Flyover Radin Inten 1", transfers: ["11Q", "JAK 39"], icons: ["icon-train.svg"], halteInfo: { type: "integrasi", halte: ["Simpang Buaran"], routes: ["11", "11M"] }, stationIntegration: { station: "Buaran", trainLines: ["C | Cikarang"] } },
+                        { name: "Carrefour Buaran", transfers: [] },
+                        { name: "SMKN 48", transfers: ["11Q", "JAK 39"] },
+                        { name: "Budhaya Santo Agustinus", transfers: ["11Q", "JAK 99"] },
+                        { name: "Kasi Dikdas 2", transfers: [] },
+                        { name: "Gedung Senam", transfers: ["11Q", "JAK 99"] },
+                        { name: "Taman UT Aheme", transfers: ["11Q"] },
+                        { name: "Masjid Jami Nurul Ain 2", transfers: ["11Q"] },
+                        { name: "Sbr. Swadaya Raya", transfers: ["11Q", "JAK 42"] },
+                        { name: "Jln. Kel. Raya", transfers: ["11Q", "JAK 02", "JAK 26", "JAK 42"], isActive: true },
+                        { name: "Masjid Jami Al Barkah 2", transfers: ["11Q", "JAK 02", "JAK 26", "JAK 42"] },
+                        { name: "Sbr. SMPN 195", transfers: ["JAK 02", "JAK 26"] },
+                        { name: "RS Duren Sawit 2", transfers: ["JAK 26"] },
+                        { name: "Gereja Santa Anna 2", transfers: ["JAK 02", "JAK 85"] },
+                        { name: "Jln. Teluk Mandar 2", transfers: ["JAK 02", "JAK 85"], isActive: true, label: "Terdekat" },
+                        { name: "Rusun Pondok Bambu", transfers: ["JAK 85"], isActive: true }
+                    ]
+                }
+            ]
         },
         {
             id: "JAK02",
